ade584a22ba2c5f187bac42d92b01cb575056262
jgray4183 <j.gray.4183@gmail.com>
2024-11-13 10:01:11 +0000

    main loop creates windows

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # import dependancys
 import pygame
+from constants import *
 
+# main
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
