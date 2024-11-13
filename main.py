5dc035db0a6a24b4b953c085a52141c0f0b1d9d6
jgray4183 <j.gray.4183@gmail.com>
2024-11-13 11:15:56 +0000

    added movement and moved player to group

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    updateable, drawable = pygame.sprite.Group(), pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     middle_width, middle_height = (SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2)
     player = Player(middle_width, middle_height)
     while running == True:
@@ -17,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
+        for item in updateable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import *
-from constants import PLAYER_RADIUS
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -17,4 +17,23 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, ("white"), self.triangle(), 2)
+        pygame.draw.polygon(screen, ("white"), self.triangle(), 1)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate((dt * -1))
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move((dt * -1))
